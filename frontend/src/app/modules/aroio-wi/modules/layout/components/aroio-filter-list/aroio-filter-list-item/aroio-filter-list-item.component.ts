002cb6e92eb6ee94a5c9d06a2729501a2822db71
Melf Stöcken <melf.stoecken@googlemail.com>
2020-04-10 12:23:25 +0200

    modal styling, build

--- frontend/src/app/modules/aroio-wi/modules/layout/components/aroio-filter-list/aroio-filter-list-item/aroio-filter-list-item.component.ts
+++ frontend/src/app/modules/aroio-wi/modules/layout/components/aroio-filter-list/aroio-filter-list-item/aroio-filter-list-item.component.ts
@@ -1,15 +1,17 @@
-import {Component, ElementRef, HostBinding, TemplateRef, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostBinding, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'aroio-filter-list-item',
   templateUrl: 'aroio-filter-list-item.component.html'
 })
-export class AroioFilterListItemComponent {
+export class AroioFilterListItemComponent implements OnInit {
 
   @HostBinding('class') class = 'aroio-filter-list__item';
   id = Math.random().toString(36).substring(7);
 
+  form: FormGroup;
 
   // Modal Specific
   modalIndex = 0;
@@ -19,18 +21,30 @@ export class AroioFilterListItemComponent {
     private modalService: BsModalService
   ){}
 
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      filterName: new FormControl('', Validators.required),
+      comment: new FormControl(''),
+      pegel: new FormControl(''),
+    });
+  }
+
   // Modal Specific
   // -----------------------
   openModal(template: TemplateRef<any>, _index: number) {
-    this.modalRef = this.modalService.show(template, {class: 'modal--lg'});
+    if (!this.modalRef) {
+      this.modalRef = this.modalService.show(template, {class: 'modal--lg'});
+    }
   }
 
   modalAccept(_index) {
     console.log('Modal Accepted');
     this.modalRef.hide();
   }
+
   modalClose(_index) {
-    console.log('Modal Closed');
     this.modalRef.hide();
+    this.modalRef = null;
   }
 }
