1e5a7afb2875a67a24f5365ddf352e27bd7dbaf3
Melf Stöcken <melf.stoecken@googlemail.com>
2020-06-29 17:28:11 +0200

    prod config, build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  api: 'http://0.0.0.0:8000',
-  socket_api: 'ws://127.0.0.1:8000',
+  api: 'http://localhost:8000',
+  socket_api: 'ws://localhost:8000',
   cdn: 'https://cdn.stillleben.media/abacus/webinterface/',
   version: '1.0',
   defaultLocale: 'de',
